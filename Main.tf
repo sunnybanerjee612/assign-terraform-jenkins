fac49011c6c73db56e15e6156f3156507186a102
sunnybanerjee612 <83915611+sunnybanerjee612@users.noreply.github.com>
2021-07-04 14:14:54 -0500

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,17 +1,26 @@
-aprovider "aws" {
- region = "eu-central-1"
- profile = "Deep"
- }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-central-1"
+}
  resource "aws_instance" "terra_instance" {
   ami           = ami-0b1deee75235aa4bb
   instance_type = "t3.micro"
-
   }
-resource “aws_s3_bucket” “bucket” {
-   Bucket = “deep7-4pm-sun”
-    acl        = “public-read”
-    tags = {
-    Name    = “My bucket”
-    Environment = “Dev”
+resource "aws_s3_bucket" "bucket" {
+  bucket = "my-tf-test1-bucket"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
