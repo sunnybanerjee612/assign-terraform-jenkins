a42421d414c6f5abd80bd45b37fd2b1b310e6b4f
sunnybanerjee612 <83915611+sunnybanerjee612@users.noreply.github.com>
2021-07-04 14:02:03 -0500

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,9 +1,9 @@
-provider "aws" {
+aprovider "aws" {
  region = "eu-central-1"
  profile = "Deep"
  }
- resource "aws_instance" "my-terraform-assignment" {
-  ami           = “ami-0b1deee75235aa4bb”
+ resource "aws_instance" "terra_instance" {
+  ami           = ami-0b1deee75235aa4bb
   instance_type = "t3.micro"
 
   }
